e7a4d77eb52a95d1b8404c380535a609ef07ebdc
priooods <priodwisembodo23@gmail.com>
2024-11-08 01:25:31 +0700

    [BE][MENU] : Add Module Menu

---
+++ backend/Modules/Master/app/Http/Controllers/MenuController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Modules\Master\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Modules\Master\Models\MMenuTab;
+
+class MenuController extends Controller
+{
+    protected $mMenuTab, $controller;
+    public function __construct(
+        Controller $controller,
+        MMenuTab $mMenuTab
+    ) {
+        $this->controller = $controller;
+        $this->mMenuTab = $mMenuTab;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->controller->resSuccess($this->mMenuTab->where('m_status_tabs_id', 2)->with('child')->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('master::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->controller->validing($request->all(), [
+            'title' => 'required|unique:m_menu_tabs,title',
+            'url' => 'required',
+            'sequence' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $this->mMenuTab->create($request->all());
+            DB::commit();
+            return $this->controller->resSuccess('CREATE MENU');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(501, $th->getMessage());
+        }
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show($id)
+    {
+        return view('master::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        return view('master::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $this->controller->validing($request->all(), [
+            'title' => 'required|unique:m_menu_tabs,title',
+            'url' => 'required',
+            'sequence' => 'required',
+            'm_status_tabs_id' => 'required',
+            'parent_id' => 'required',
+            'icon' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $this->mMenuTab->where('id', $id)->update([
+                'title' => $request->title,
+                'url' => $request->url,
+                'sequence' => $request->sequence,
+                'icon' => $request->icon,
+                'm_status_tabs_id' => $request->m_status_tabs_id,
+                'parent_id' => $request->parent_id,
+            ]);
+            DB::commit();
+            return $this->controller->resSuccess('UPDATED MENU');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(501, $th->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        try {
+            DB::beginTransaction();
+            $this->mMenuTab->where('id', $id)->delete();
+            DB::commit();
+            return $this->controller->resSuccess("DELETED MENU");
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(501, $th->getMessage());
+        }
+    }
+}
---
+++ backend/Modules/Master/database/migrations/2024_11_07_174925_create_m_menu_tabs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_menu_tabs', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->string('title');
+            $table->string('url');
+            $table->string('icon')->nullable();
+            $table->tinyInteger('m_status_tabs_id')->default(3)->comment('3 = not active, 2 = active');
+            $table->mediumInteger('sequence');
+            $table->integer('parent_id')->nullable();
+            $table->foreign('m_status_tabs_id')->on('m_status_tabs_id')->references('id')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_menu_tabs');
+    }
+};
